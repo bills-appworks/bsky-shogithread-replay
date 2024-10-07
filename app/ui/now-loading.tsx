50e5821f90a9bd585d2fc676c65b901072dd29d4
bills-appworks <bills.appworks@gmail.com>
2024-10-07 22:42:22 +0900

    20241007-04

--- app/ui/now-loading.tsx
+++ app/ui/now-loading.tsx
@@ -1,5 +1,5 @@
 /**
- * @author bills-appworks
+ * @author bills-appworks https://bsky.app/profile/did:plc:lfjssqqi6somnb7vhup2jm5w
  * @copyright bills-appworks 2024
  * @license This software is released under the MIT License. http://opensource.org/licenses/mit-license.php
  */
