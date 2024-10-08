a70f7939da65b8c5d4a9bfd9b025b56aeb533f63
bills-appworks <bills.appworks@gmail.com>
2024-10-08 20:20:14 +0900

    modify: Google Analytics cookie属性調整

--- app/layout.tsx
+++ app/layout.tsx
@@ -52,7 +52,9 @@ export default function RootLayout({
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
 
-              gtag('config', 'G-GLV44TDZ0X');
+              gtag('config', 'G-GLV44TDZ0X', {
+                'cookie_flags': 'SameSite=None;Secure'
+              });
             `,
           }}
         />
