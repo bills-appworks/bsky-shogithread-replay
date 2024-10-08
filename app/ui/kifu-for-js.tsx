cb3276afc2c8d55cb8941456c340bbde73a2e39a
bills-appworks <bills.appworks@gmail.com>
2024-10-08 22:48:20 +0900

    fix: 棋譜コンポーネントにmax-height適用

--- app/ui/kifu-for-js.tsx
+++ app/ui/kifu-for-js.tsx
@@ -8,12 +8,13 @@ import { KifuLite } from 'kifu-for-js';
 import { KifuStoreState } from '@/app/lib/common';
 
 const KifuForJS = ({ kifuStoreState }: { kifuStoreState: KifuStoreState }) => {
-    return (
+  return (
     <KifuLite
       kifuStore={ kifuStoreState.kifuStore }
       style={{
         border: "1px black solid",
-        backgroundColor: "rgba(255, 255, 255, 0.2)"
+        backgroundColor: "rgba(255, 255, 255, 0.2)",
+        maxHeight: "533px",
       }}
     />
   );
